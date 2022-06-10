a703f6a060cb3dcd850e537e062fe2b843f7a24a
arunraju359 <arunraju359@gmail.com>
2022-06-10 08:12:42 +0530

    committing the file

---
+++ 4-fun.sh
@@ -0,0 +1,10 @@
+fun()
+{
+    echo "Hi this is sample function program 1"
+    sleep 5
+    echo "Hi this is sample function program 2"
+    sleep 5
+    echo "Hi this is sample function program 3"
+    sleep 5
+    echo "Hi this is sample function program 4"
+}
