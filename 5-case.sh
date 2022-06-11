f51154823000cdd6f4c0fd1fc4749a7296a37d59
arunraju359 <arunraju359@gmail.com>
2022-06-11 10:13:13 +0530

    committing the file

--- 5-case.sh
+++ 5-case.sh
@@ -7,7 +7,7 @@ case $ACTION in
     echo -e "\e[0;35m Startting the service \e[0m"
     ;;
     stop)
-    echo -e "\e[0;37m Stopping the service \e[0m"
+    echo -e "\e[0;34m Stopping the service \e[0m"
     ;;
     *)
     echo "print valid input"
