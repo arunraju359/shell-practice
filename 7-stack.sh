f34b6d7804c8c23375b489704ef3ead315714e34
arunraju359 <arunraju359@gmail.com>
2022-06-13 10:49:59 +0530

    committing the file

--- 7-stack.sh
+++ 7-stack.sh
@@ -6,7 +6,7 @@ if [ "$ID" -ne 0 ]; then
 fi
 echo -n "Installing Httpd : "
 yum install httpd &> /tmp/stack.logs
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
     echo -e "\e[32m Success [0m"
 else
     echo -e "\e[31m Failure [0m"
