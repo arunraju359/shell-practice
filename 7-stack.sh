44b4e3dbe73e866e272e7864a69f8efb2b5b9a8e
arunraju359 <arunraju359@gmail.com>
2022-06-13 11:02:57 +0530

    committing the file

--- 7-stack.sh
+++ 7-stack.sh
@@ -4,10 +4,20 @@ if [ "$ID" -ne 0 ]; then
     echo -e "\e[31m You need to perform the operation as a root user \e[0m"
     exit 1
 fi
-echo -n "Installing Httpd : "
-yum install httpd &> /tmp/stack.logs
-if [ $? -eq 0 ]; then
+stat()
+{
+    if [ $1 -eq 0 ]; then
     echo -e "\e[32m Success \e[0m"
-else
+    else
     echo -e "\e[31m Failure \e[0m"
 fi
+
+}
+echo -n "Installing Httpd : "
+yum install httpd &> /tmp/stack.logs
+stat $?
+
+echo -n "Update the reverse Proxy Congiguration"
+echo 'ProxyPass "/student" "http://APP-SERVER-IPADDRESS:8080/student"
+ProxyPassReverse "/student"  "http://APP-SERVER-IPADDRESS:8080/student"' > etc/httpd/conf.d/proxy.conf
+stat $?
