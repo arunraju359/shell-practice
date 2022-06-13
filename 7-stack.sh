ec975c363b21cba2eef54714b92e8f2d6459ef5b
arunraju359 <arunraju359@gmail.com>
2022-06-13 10:49:25 +0530

    committing the file

--- 7-stack.sh
+++ 7-stack.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 ID=$(id -u)
 if [ "$ID" -ne 0 ]; then
-    echo -e "\e[32m You need to perform the operation as a root user \e[0m"
+    echo -e "\e[31m You need to perform the operation as a root user \e[0m"
     exit 1
 fi
+echo -n "Installing Httpd : "
 yum install httpd &> /tmp/stack.logs
 if [$? -eq 0]; then
-    echo "Success"
+    echo -e "\e[32m Success [0m"
 else
-    echo "Failure"
+    echo -e "\e[31m Failure [0m"
 fi
